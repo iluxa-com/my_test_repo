31eeb41e3fcc979d1138b9f049771e0b6ab3dc93
iluxa-com <iluxa_com@mail.ru>
2015-02-04 16:42:51 +0300

    Create test.js

---
+++ test.js
@@ -0,0 +1,8 @@
+(function(w,d,s,l,i){
+    var h = w.location.hostname;
+    if(h.indexOf('upsource.jetbrains.com')==-1&&h.indexOf('upsource.labs.intellij.net')==-1)return;
+    w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    '//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-WD7DHT');
