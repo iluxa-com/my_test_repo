25cc4a7646491f847e0291175d6bbc6285b38a2b
iluxa-com <iluxa_com@mail.ru>
2015-04-16 12:35:16 +0300

    Update test.js

--- test.js
+++ test.js
@@ -1,6 +1,7 @@
+//comment
 (function(w,d,s,l,i){
     var h = w.location.hostname;
-    if(h.indexOf('upsource.jetbrains.com')==-1&&h.indexOf('upsource.labs.intellij.net')==-1)return;
+    if(h.indexOf('upsource.jetbrains.com')==-&&h.indexOf('upsource.labs.intellij.net')==-1)return;
     w[l]=w[l]||[];w[l].push({'gtm.start':
     new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
     j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
